7b15ca873963885ecebad79b3d4ed0e8754778cf
mikeberon <mikeandrew.beron@gmail.com>
2022-06-24 10:39:51 +0800

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:1234',
+    baseUrl: "https://mcstaging.godfreys.com.au/",
     $schema: "https://on.cypress.io/cypress.schema.json",
     projectId: "mimhrk",
     viewportWidth: 1920,
